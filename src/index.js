23a2caface4c631422c98d46ccd92e201191b350
Kaash Desai <46506637+kaashdesai1@users.noreply.github.com>
2022-02-14 11:21:40 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import * as serviceWorker from './serviceWorker';
+
+import store from './reduxCtrl/store';
+
+import App from './App';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'),
+);
+
+serviceWorker.unregister();
+
+
